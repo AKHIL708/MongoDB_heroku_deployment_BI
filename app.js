146f716c4b2458f58f019526c6a87a675dcbc31b
AKhil Nayak <aakhiljay06@gmail.com>
2022-08-20 12:48:47 +0530

    changes done again in app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 1010;
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 1010;
 // require("./db/conn");
 // const studentData = require("./model/Structure");
 // app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("hello from the home page");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello from the home page");
+// });
 // app.get("/studentList", async (req, res) => {
 //   try {
 //     const gettingStudentData = await studentData.find();
@@ -31,7 +31,19 @@ app.get("/", (req, res) => {
 //     res.send(err);
 //   }
 // });
-app.listen(port, "127.0.0.1", () => {
-  console.log(`port listening at ${port}`);
-});
+// app.listen(port, "127.0.0.1", () => {
+//   console.log(`port listening at ${port}`);
+// });
 
+// const data = require("./student/getDetails.json");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 1010
+app.get("/",(req,res)=>{
+    res.send("api is running..");
+})
+
+// app.get("/student/getDetails", function (req, res) {
+//     res.send(data);
+// });
+app.listen(port, console.log(`server runnig..... `));
